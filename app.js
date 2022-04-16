3c50c19ce34ebe8409c86fe10b601b975be16c50
bakhtiyorjondadajonov <bakhtiyorjondadajonov@gmail.com>
2022-04-16 16:55:57 +0900

    Better Errors and Refactoring

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
+//---------Error handlers
+const AppError = require('./utilities/appError');
+const globalErrorHandler = require('./controllers/errorController');
+//---------------------------//
 dotenv.config({ path: './config.env' });
 const tourRouter = require('./routes/toursRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -8,24 +12,11 @@ app.use(express.json()); //MODDLEWARE
 app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+//Implementing a Global Error Handling Middleware
 app.all('*', (req, res, next) => {
-  //   res.status(404).json({
-  //     status: 'fail',
-  //     message: `Cannot find ${req.originalUrl} on this server!`,
-  //   });
-  const err = new Error(`Cannot find ${req.originalUrl} on this server!`);
-  err.statusCode = 404;
-  err.status = 'fail';
-  next(err);
+  next(new AppError(`Cannot find ${req.originalUrl} on this server!`, 404));
 });
 // ERRROR HANDLING MIDDLEWARE | we use this middleware to handle errors on central middleware,I mean on this middleware
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 module.exports = app;
