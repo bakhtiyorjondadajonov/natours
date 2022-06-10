f564359f8617d66e2e2e08c96e7b6da837043f10
bakhtiyorjondadajonov <bakhtiyorjondadajonov@gmail.com>
2022-06-11 00:04:03 +0900

    a

--- models/userModule.js
+++ models/userModule.js
@@ -4,7 +4,7 @@ const validator2 = require('validator');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
